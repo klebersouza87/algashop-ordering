efbeb9b92dcc1035ce230f10e543006df12f2a8c
Kleber Souza <kleber.souza@any.mail.com.br>
2026-03-25 08:21:04 -0300

    Added some value objects.

---
+++ src/main/java/com/algaworks/algashop/ordering/domain/valueobject/LoyaltyPoints.java
@@ -0,0 +1,36 @@
+package com.algaworks.algashop.ordering.domain.valueobject;
+
+import java.util.Objects;
+
+public record LoyaltyPoints(Integer value) implements Comparable<LoyaltyPoints> {
+
+    public LoyaltyPoints(Integer value) {
+        Objects.requireNonNull(value);
+        if(value < 0) {
+            throw new IllegalArgumentException("Value cannot be negative.");
+        }
+        this.value = value;
+    }
+
+    public LoyaltyPoints add(Integer value) {
+        return add(new LoyaltyPoints(value));
+    }
+
+    public LoyaltyPoints add(LoyaltyPoints loyaltyPoints) {
+        Objects.requireNonNull(loyaltyPoints);
+        if (loyaltyPoints.value() < 0) {
+            throw new IllegalArgumentException("Value cannot be negative.");
+        }
+        return new LoyaltyPoints(this.value() + loyaltyPoints.value());
+    }
+
+    @Override
+    public String toString() {
+        return value.toString();
+    }
+
+    @Override
+    public int compareTo(LoyaltyPoints loyaltyPoints) {
+        return this.value.compareTo(loyaltyPoints.value);
+    }
+}
